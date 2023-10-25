5e4fc0bb945f03c9c5cb7875aa7ffdcbc76cfb90
Cola Ibañez Ignacio Bautista <i.cola@etec.local>
2023-10-25 13:00:33 -0300

    7puntos

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,22 @@
-import {Router} from "express";
-
+import { Router, Request, Response } from "express";
 const router: Router = Router();
 
-router.get ("/", (_,res) =>{
-
-    res.send ("si ves esto es porque funciono la muda y river sigue siendo el mas grande");
+router.get("/", (_, res) => {
+    res.send("Si ves esto es porque funcionó la ruta y River sigue siendo el más grande");
 });
 
-export default router
+let productoyMercancia = [
+    { nombre: "producto1", modelo: "modelo1", precio: 1, paisOrigen: "pais1" },
+    { nombre: "producto2", modelo: "modelo2", precio: 2, paisOrigen: "pais2" },
+    { nombre: "producto3", modelo: "modelo3", precio: 3, paisOrigen: "pais3" },
+];
+
+//punto (1)
+router.get("/productos", (_: Request, res: Response) => {
+    res.send(productoyMercancia);
+    
+});
+//punto (2)
+
+export default router;
+
