8eafddd34d4be2972a918d5bae58b6029711b1d1
Cola Ibañez Ignacio Bautista <i.cola@etec.local>
2023-10-27 12:59:47 -0300

    avances

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,13 +2,14 @@ import { Router, Request, Response } from "express";
 const router: Router = Router();
 
 router.get("/", (_, res) => {
-    res.send("Si ves esto es porque funcionó la ruta y River sigue siendo el más grande");
+    res.send("El server funca");
 });
 
+
 let productoyMercancia = [
-    { nombre: "producto1", modelo: "modelo1", precio: 1, paisOrigen: "pais1" },
-    { nombre: "producto2", modelo: "modelo2", precio: 2, paisOrigen: "pais2" },
-    { nombre: "producto3", modelo: "modelo3", precio: 3, paisOrigen: "pais3" },
+    { nombre: "mate", modelo: "yerba", Precio: 90, paisOrigen: "Argentina" },
+    { nombre: "cafe", modelo: "grano", Precio: 250, paisOrigen: "Chile" },
+    { nombre: "te", modelo: "saco", Precio: 360, paisOrigen: "Peru" },
 ];
 
 //punto (1)
@@ -17,6 +18,62 @@ router.get("/productos", (_: Request, res: Response) => {
     
 });
 //punto (2)
+router.get("/productosMas100", (_: Request, res: Response) => {
+    const productosFiltrados = productoyMercancia.filter(producto => producto.Precio > 100);
+    res.send(productosFiltrados);
+});
+
+//punto(3)
+
+
+//punto(4)
+router.get('/productos/:modelo', (req: Request, res: Response) => {
+    const {modelo}= req.params;
+    const longitud = productoyMercancia.length;
+    productoyMercancia = productoyMercancia.filter(producto => producto.modelo !== modelo);
+
+    if (productoyMercancia.length === longitud){
+        res.status(404).send ('No existe el producto');
+        console.log('no existe elproducto');
+
+    }else {
+        res.send('producto eliminado exitosamente');
+        console.log ('producto eliminado exitosamente')
+    }
+});
+
+
+
+//punto(5)
+
+router.get('/productos/porPais/:pais',(req: Request, res: Response) =>{
+    const {pais}= req.params;
+    const productoaEncontrar = productoyMercancia.find(productoyMercancia => productoyMercancia.paisOrigen === pais);
+
+    if (productoaEncontrar ){
+        res.json(productoaEncontrar);
+
+    } else{
+        res.status(404).send('no se encontro el producto');
+    }
+
+});
+
+//punto(6)
+router.get('/productos/porPrecio/:Precio',(req: Request, res: Response) =>{
+    const {Precio}= req.params;
+    const precioaEncontrar = productoyMercancia.filter(productoyMercancia =>productoyMercancia.Precio === parseInt(Precio));
+
+    if (precioaEncontrar.length !=0 ){
+        res.json(precioaEncontrar);
+
+    } else{
+        res.status(404).send('PRECIO NO ENCONTRADO');
+    }
+
+});
 
 export default router;
+
+//punto(7)
 
